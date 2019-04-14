f239e7548a8938cce20f030d0489420219e81c43
Sergey <47604847+SEREGA2976@users.noreply.github.com>
2019-04-14 14:11:13 +0500

    Добавьте файлы проекта.

---
+++ WebStore/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebStore
+{
+    public class Startup
+    {
+
+        public IConfiguration Configuration {get;}
+
+        public Startup(IConfiguration Configuration)
+        {
+            this.Configuration = Configuration;
+        }
+        public void ConfigureServices(IServiceCollection services){ }
+       
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var response = Configuration["CustomClientResponse"];
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(response);
+            });
+        }
+    }
+}
